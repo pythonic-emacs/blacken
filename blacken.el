9caa3e1ceae52c21c1c4f00c410485803b7a3c2b
Todd Gamblin <tgamblin@llnl.gov>
2021-04-05 21:26:33 -0700

    make blacken.el work with global whitespace mode
    [Global whitespace mode](http://ergoemacs.org/emacs/whitespace-mode.html)
    colors overlong lines purple, and `blacken.el` should set
    `whitespace-line-column` to agree with `blacken-line-length`.
    
    This sets the variable when blacken mode is enabled.

--- blacken.el
+++ blacken.el
@@ -165,9 +165,15 @@ Show black output, if black exit abnormally and DISPLAY is t."
   "Automatically run black before saving."
   :lighter " Black"
   (if blacken-mode
-      (when (or (not blacken-only-if-project-is-blackened)
-                (blacken-project-is-blackened))
-        (add-hook 'before-save-hook 'blacken-buffer nil t))
+      (progn
+        ;; add before save hook to automatically run black
+        (when (or (not blacken-only-if-project-is-blackened)
+                  (blacken-project-is-blackened))
+          (add-hook 'before-save-hook 'blacken-buffer nil t))
+        ;; show overlong lines correctly when using whitespace mode
+        (if blacken-line-length
+            (setq whitespace-line-column blacken-line-length)
+          (setq whitespace-line-column 88)))
     (remove-hook 'before-save-hook 'blacken-buffer t)))
 
 (provide 'blacken)
